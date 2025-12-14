e4a30fcbf4bcfdb838664a8d51e1e6eae670f4f4
kiran <kiran@technource.in>
2025-12-14 12:00:38 +0530

    - validation update.

--- app/Http/Requests/V1/Accounts/StoreRequest.php
+++ app/Http/Requests/V1/Accounts/StoreRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\V1\Accounts;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreRequest extends FormRequest
 {
@@ -23,7 +24,14 @@ class StoreRequest extends FormRequest
     {
       return [
         'nickname'      => 'required|string|max:255',
-        'client_id'     => 'required|string|max:255',
+        'client_id' => [
+          'required',
+          'string',
+          'max:255',
+          Rule::unique('accounts', 'client_id')->where(fn ($query) =>
+              $query->where('user_id', auth()->id())
+            ),
+        ],
         'pin'           => 'required|max:4',
         'api_key'       => 'required|string|max:255',
         'client_secret' => 'nullable|string|max:255',
