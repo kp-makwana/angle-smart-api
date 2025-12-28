ddec40693acd91b2b1a8e8ac335a54dc43e7b1fa
kiran <kiran@technource.in>
2025-12-28 17:16:56 +0530

    - update.

--- resources/views/layouts/sections/navbar/navbar-partial.blade.php
+++ resources/views/layouts/sections/navbar/navbar-partial.blade.php
@@ -239,7 +239,7 @@ use Illuminate\Support\Facades\Route;
           <div class="dropdown-header d-flex align-items-center py-3">
             <h6 class="mb-0 me-auto">Notification</h6>
             <div class="d-flex align-items-center h6 mb-0">
-              <span class="badge bg-label-primary me-2">8 New</span>
+              <span class="badge bg-label-primary me-2">1 New</span>
               <a href="javascript:void(0)" class="dropdown-notifications-all p-2 btn btn-icon" data-bs-toggle="tooltip"
                 data-bs-placement="top" title="Mark all as read"><i
                   class="icon-base ti tabler-mail-opened text-heading"></i></a>
@@ -249,6 +249,26 @@ use Illuminate\Support\Facades\Route;
         <li class="dropdown-notifications-list scrollable-container">
           <ul class="list-group list-group-flush">
             <li class="list-group-item list-group-item-action dropdown-notifications-item">
+              <div class="d-flex">
+                <div class="flex-shrink-0 me-3">
+                  <div class="avatar">
+                    <img src="{{ asset('assets/img/avatars/1.png') }}" alt class="rounded-circle" />
+                  </div>
+                </div>
+                <div class="flex-grow-1">
+                  <h6 class="mb-1 small">New Message ✉️</h6>
+                  <small class="mb-1 d-block text-body">Your Account Successfully Created</small>
+                  <small class="text-body-secondary">Now</small>
+                </div>
+                <div class="flex-shrink-0 dropdown-notifications-actions">
+                  <a href="javascript:void(0)" class="dropdown-notifications-read"><span
+                      class="badge badge-dot"></span></a>
+                  <a href="javascript:void(0)" class="dropdown-notifications-archive"><span
+                      class="icon-base ti tabler-x"></span></a>
+                </div>
+              </div>
+            </li>
+            {{--<li class="list-group-item list-group-item-action dropdown-notifications-item">
               <div class="d-flex">
                 <div class="flex-shrink-0 me-3">
                   <div class="avatar">
@@ -279,26 +299,6 @@ use Illuminate\Support\Facades\Route;
                   <h6 class="mb-1 small">Charles Franklin</h6>
                   <small class="mb-1 d-block text-body">Accepted your connection</small>
                   <small class="text-body-secondary">12hr ago</small>
-                </div>
-                <div class="flex-shrink-0 dropdown-notifications-actions">
-                  <a href="javascript:void(0)" class="dropdown-notifications-read"><span
-                      class="badge badge-dot"></span></a>
-                  <a href="javascript:void(0)" class="dropdown-notifications-archive"><span
-                      class="icon-base ti tabler-x"></span></a>
-                </div>
-              </div>
-            </li>
-            <li class="list-group-item list-group-item-action dropdown-notifications-item marked-as-read">
-              <div class="d-flex">
-                <div class="flex-shrink-0 me-3">
-                  <div class="avatar">
-                    <img src="{{ asset('assets/img/avatars/2.png') }}" alt class="rounded-circle" />
-                  </div>
-                </div>
-                <div class="flex-grow-1">
-                  <h6 class="mb-1 small">New Message ✉️</h6>
-                  <small class="mb-1 d-block text-body">You have new message from Natalie</small>
-                  <small class="text-body-secondary">1h ago</small>
                 </div>
                 <div class="flex-shrink-0 dropdown-notifications-actions">
                   <a href="javascript:void(0)" class="dropdown-notifications-read"><span
@@ -430,7 +430,7 @@ use Illuminate\Support\Facades\Route;
                       class="icon-base ti tabler-x"></span></a>
                 </div>
               </div>
-            </li>
+            </li>--}}
           </ul>
         </li>
         <li class="border-top">
